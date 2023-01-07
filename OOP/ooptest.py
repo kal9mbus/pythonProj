2ba76f197486a02ae9573013f69bd1e2598e36c7
kal9mbus <50074409+kal9mbus@users.noreply.github.com>
2023-01-07 16:07:44 +1100

    great job

--- OOP/ooptest.py
+++ OOP/ooptest.py
@@ -8,3 +8,34 @@ class Cat:
 
 tom = Cat('Tom')
 walt = Cat("Walt", 'britain', 2, 'black')
+
+class Laptop():
+    def __init__(self, brand, model, price):
+        self.brand = brand
+        self.model = model
+        self.price = price
+
+hp = Laptop('hp','15-3321', 5600)
+print(hp.price)
+    
+class SoccerPlayer():
+    def __init__(self, name, surname, goals=0, assists=0 ):
+        self.name = name
+        self.surname = surname
+        self.goals = goals
+        self.assists = assists
+    def score(self, goals = 1):
+        self.goals = goals
+    def make_assists(self, assists = 1):
+        self.assists = assists
+    def statistics(self):
+        print(f"{self.name} {self.surname}  - голы {self.goals}, передачи {self.assists}")
+cris = SoccerPlayer("Cristiano", "Ronaldo")
+
+cris.score(2)
+cris.make_assists(2)
+cris.statistics()
+messi = SoccerPlayer('Lionel', "Messi")
+messi.make_assists(20)
+messi.score(30)
+messi.statistics()
